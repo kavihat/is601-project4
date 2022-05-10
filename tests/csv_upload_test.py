230fc95e3151a9d257363aae44585629258e7f6d
Kavitha <kavita.kannanunny@gmail.com>
2022-05-10 10:24:41 -0400

    Added transaction csv upload test

--- tests/csv_upload_test.py
+++ tests/csv_upload_test.py
@@ -8,24 +8,25 @@ from app import db, config
 from app.db.models import User, Song
 
 
-# def test_upload_success(application, client):
-#     with application.app_context():
-#         email = 'testuser@email.com'
-#         password = 'testtest'
-#         user = User.query.filter_by(email=email).first()
-#         assert user is None
-#
-#         client.post("/register", data=dict(email=email, password=password, confirm=password), follow_redirects=True)
-#         client.post("/login", data=dict(email=email, password=password),
-#                                follow_redirects=True)
-#
-#         upload_file = 'test_music.csv'
-#         with open(upload_file, 'w') as f:
-#             f.write("Name,Artist,Composer,Album,Grouping,Work,Movement Number,Movement Count,Movement Name,Genre\n")
-#             f.write("Down,Marian Hill,Jeremy Lloyd & Samantha Gongol,ACT ONE,,,,,,Electronic")
-#
-#         client.post("/songs/upload", data=dict(file=open(upload_file, 'rb')),follow_redirects=True)
-#         assert db.session.query(Song).count() == 1
+def test_upload_success(application, client):
+    with application.app_context():
+        email = 'testuser@email.com'
+        password = 'testtest'
+        user = User.query.filter_by(email=email).first()
+        assert user is None
+
+        client.post("/register", data=dict(email=email, password=password, confirm=password), follow_redirects=True)
+        client.post("/login", data=dict(email=email, password=password),
+                               follow_redirects=True)
+
+        upload_file = 'test_transaction.csv'
+        with open(upload_file, 'w') as f:
+
+            f.write("AMOUNT,TYPE\n")
+            f.write("2000,CREDIT")
+
+        client.post("/songs/upload", data=dict(file=open(upload_file, 'rb')),follow_redirects=True)
+        assert db.session.query(Song).count() == 1
 
 
 def test_upload_failure(application, client):
