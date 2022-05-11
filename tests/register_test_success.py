791bc6f666d638c52dcac78edc8ca95f7b827160
Kavitha <kavita.kannanunny@gmail.com>
2022-05-11 12:34:10 -0400

    added register test succes

--- tests/register_test_success.py
+++ tests/register_test_success.py
@@ -17,15 +17,4 @@ def test_register_success(application, client):
         assert user is not None
         assert response.status_code == 200
 
-def test_register_failure(application, client):
-    with application.app_context():
-        email = 'testuser@email.com'
-        password = ''
-        user = User.query.filter_by(email=email).first()
-        assert user is None
-
-        response = client.post("/register", data=dict(email=email, password=password, confirm=password), follow_redirects=True)
-
-        user = User.query.filter_by(email=email).first()
-        assert user is None
 
