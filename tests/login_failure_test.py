9f4e405ab04ae9cb801ace8bf14f600fdc6c3c99
Kavitha <kavita.kannanunny@gmail.com>
2022-05-11 10:21:46 -0400

    login test for unregistered user

--- tests/login_failure_test.py
+++ tests/login_failure_test.py
@@ -1,10 +1,12 @@
 from app import User
+
+
 def test_login_failure(application, client):
     with application.app_context():
         email = 'wronguser@email.com'
         password = 'wrongpass'
-        user = User.query.filter_by(email=email).first()
         response = client.post("/login", data=dict(email=email, password=password),
                                follow_redirects=True)
         user = User.query.filter_by(email=email).first()
         assert user is None
+        assert response.status_code == 302
